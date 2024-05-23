cddfddc68fb546d5c6156c0c99c035e8c0138846
quirk <tracy.bowersox@gmail.com>
2024-05-23 10:14:49 -0600

    These changes implement buttons to highlight and un-highlight paragraphs, pushing and slicing entries to and from an array. The array is checked when styles are updated. There is also a new button to clear the array, but currently highlights are not cleared until styles are updated again.
    Small changes were also made to CSS classes to better accomodate
    the new highlight buttons

--- fanfic_reader/public/websocket_reader.js
+++ fanfic_reader/public/websocket_reader.js
@@ -5,6 +5,7 @@ const socket = new WebSocket(`ws://${DOMAIN}:${PORT}/`, );
 let socketOpen = false;
 let currentIndex = 0;
 let maxIndex = null;
+var markedDialogue = [];
 
 const TEXT_CONTAINER_SELECTOR = '#chatlog'
 const CLIENT_LIST_SELECTOR = '.clientList';
@@ -238,6 +239,22 @@ function formatTextToParagraphs(htmlText, activeIndex) {
     return styleActiveAndInactiveParagraphs(paragraphs, activeIndex);
 }
 
+function markParagraph(textSpan, index) {
+    textSpan.classList.toggle('markedP');
+    if (markedDialogue.includes(index) == true) {
+        markedDialogue.splice(markedDialogue.indexOf(index), 1);
+        console.log('Paragraph at index', index, ' removed from markedDialogue[]');
+    } else {
+        markedDialogue.push(index);
+        console.log('Paragraph at index', index, ' added to markedDialogue[]');
+    }
+    console.log(markedDialogue);
+}
+
+function clearMarks() {
+    markedDialogue.length = 0; 
+    // TODO: Update styles
+}
 /**
  * 
  * @param {Array<string>} pHTMLStrings 
@@ -259,9 +276,19 @@ function styleActiveAndInactiveParagraphs(pHTMLStrings, activeIndex) {
             paragraph.appendChild(selectParagraphArrow);
         }
 
+        const markDialogueButton = document.createElement('button');
+        markDialogueButton.className = 'p_marker';
+        markDialogueButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-highlighter" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.096.644a2 2 0 0 1 2.791.036l1.433 1.433a2 2 0 0 1 .035 2.791l-.413.435-8.07 8.995a.5.5 0 0 1-.372.166h-3a.5.5 0 0 1-.234-.058l-.412.412A.5.5 0 0 1 2.5 15h-2a.5.5 0 0 1-.354-.854l1.412-1.412A.5.5 0 0 1 1.5 12.5v-3a.5.5 0 0 1 .166-.372l8.995-8.07zm-.115 1.47L2.727 9.52l3.753 3.753 7.406-8.254zm3.585 2.17.064-.068a1 1 0 0 0-.017-1.396L13.18 1.387a1 1 0 0 0-1.396-.018l-.068.065zM5.293 13.5 2.5 10.707v1.586L3.707 13.5z"/></svg>';
+        paragraph.appendChild(markDialogueButton);
+
         const text = document.createElement('span');
         text.innerHTML = p;
 
+        markDialogueButton.onclick = () => markParagraph(text, i);
+
+       if (markedDialogue.includes(i)) {
+            text.classList.add('markedP');
+        }
         paragraph.appendChild(text);
         return paragraph;
     });
